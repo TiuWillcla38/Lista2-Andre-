7d62b5f2fa15ac941d38c4d02e1e815b4a7dc5b9
TiuWillcla38 <54819606+TiuWillcla38@users.noreply.github.com>
2019-09-10 12:40:46 -0300

    Update main.c

--- exc6/exc6/main.c
+++ exc6/exc6/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(void){
+	int vitoriaVermelha, empateVermelha, derrotaVermelha;
+	int vitoriaAzul, empateAzul, derrotaAzul;
+	printf("Equipe Vermelha");
+	printf("\nDigite a quantidade de vitorias da equipe vermelha: ");
+	scanf("%d", &vitoriaVermelha);
+	printf("\nDigite a quantidade de empates da equipe vermelha: ");
+	scanf("%d",&empateVermelha);
+	printf("\nDigite a quantidade de derrotas da equipe vermelha: ");
+	scanf("%d",&derrotaVermelha);
+	printf("\nDigite a quantidade de vitorias da equipe azul: ");
+	scanf("%d",&vitoriaAzul);
+	printf("\nDigite a quantidade de empates da equipe azul: ");
+	scanf("%d",&empateAzul);
+	printf("\nDigite a quantidade de derrotas da equipe azul: ");
+	scanf("%d",&derrotaAzul);
+	printf("\n\nEquipe Vermelha ---- Equipe Azul");
+	printf("\n  Vitorias: %d    ---- Vitorias: %d",vitoriaVermelha,vitoriaAzul);
+	printf("\n  Empates:  %d    ---- Empates:  %d",empateVermelha,empateAzul);
+	printf("\n  Derrotas: %d    ---- Derrotas: %d",derrotaVermelha,derrotaAzul);
+	printf("\n  Pontos:   %d    ---- Pontos:   %d",(vitoriaVermelha*3)+(empateVermelha), (vitoriaAzul*3)+(empateAzul));
 }
